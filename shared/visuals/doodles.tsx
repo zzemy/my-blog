775149f0c69eebc4763b2abecde0b298f2ab0098
zhalio <1992107794@qq.com>
2026-04-11 21:35:00 +0800

    fix(typo): fix strokeLinejoin typo in doodles

--- shared/visuals/doodles.tsx
+++ shared/visuals/doodles.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 export function HandDrawnStar(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinelinejoin="round" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <path d="M12 2c-.5 4-3 7-7 8 4 1 6.5 4 7 8 .5-4 3-7 7-8-4-1-6.5-4-7-8z" />
     </svg>
   )
@@ -10,7 +10,7 @@ export function HandDrawnStar(props: React.SVGProps<SVGSVGElement>) {
 
 export function HandDrawnArrow(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinelinejoin="round" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <path d="M3 12c4-2 8-1 14 0" />
       <path d="M13 7l5 5-5 5" />
     </svg>
@@ -19,7 +19,7 @@ export function HandDrawnArrow(props: React.SVGProps<SVGSVGElement>) {
 
 export function HandDrawnUnderline(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 10" preserveAspectRatio="none" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinelinejoin="round" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 10" preserveAspectRatio="none" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <path d="M2 5c25-3 50 2 95-1" />
     </svg>
   )
@@ -27,7 +27,7 @@ export function HandDrawnUnderline(props: React.SVGProps<SVGSVGElement>) {
 
 export function HandDrawnCloud(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinelinejoin="round" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
     </svg>
   )
@@ -35,7 +35,7 @@ export function HandDrawnCloud(props: React.SVGProps<SVGSVGElement>) {
 
 export function HandDrawnSmiley(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinelinejoin="round" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <circle cx="12" cy="12" r="10" />
       <path d="M8 14s1.5 2 4 2 4-2 4-2" />
       <line x1="9" y1="9" x2="9.01" y2="9" />
@@ -46,7 +46,7 @@ export function HandDrawnSmiley(props: React.SVGProps<SVGSVGElement>) {
 
 export function HandDrawnScribble(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinelinejoin="round" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <path d="M20 20c20 10 0 40 20 50 15 7.5 30-10 40 5 8 12-10 20 0 30" />
     </svg>
   )
